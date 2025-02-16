4473716c4e2ed7896d8eda583f32ab1cbd55e039
RukaiyaMustafizu <rukaiyamustafizu@icloud.com>
2025-02-17 00:47:22 +0100

    Added some java script

--- js.js
+++ js.js
@@ -1,1 +1,11 @@
-
+function changeTheme() {
+  let body = document.querySelector("body");
+  body.classList.toggle("dark");
+  if (body.classList.contains("dark")) {
+    themeButton.textContent = "Light Theme";
+  } else {
+    themeButton.textContent = "Dark Theme";
+  }
+}
+let themeButton = document.querySelector(".darkTheme");
+themeButton.addEventListener("click", changeTheme);
