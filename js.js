ddb8a925f4cbd7ec6fee41bed9b10bff4cc0c96d
RukaiyaMustafizu <rukaiyamustafizu@icloud.com>
2025-02-17 19:00:05 +0100

    tried different codes but no success

--- js.js
+++ js.js
@@ -89,30 +89,6 @@ function searchOnGoogle() {
   }
 }
 
-function displayForecast() {
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
-  let forecastHtml = "";
-
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
-      <div class="weather-forecast-day">
-        <div class="weather-forecast-date">${day}</div>
-        <div class="weather-forecast-icon">🌤️</div>
-        <div class="weather-forecast-temperatures">
-          <div class="weather-forecast-temperature">
-            <strong>15º</strong>
-          </div>
-          <div class="weather-forecast-temperature">9º</div>
-        </div>
-      </div>
-    `;
-  });
-  let forecastElement = document.querySelector("#forecast");
-  forecastElement.innerHTML = forecastHtml;
-}
-
 // Add event listener to the search form
 let searchFormElement = document.querySelector(".searchForm form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
